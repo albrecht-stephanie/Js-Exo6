b4e8496efa40e8326efab2541240982a6e3986b1
AS <albrecht.stephanie@outlook.fr>
2019-10-23 23:29:03 +0200

    mise en page et verif saisie

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,12 @@
 function controle(){
   var firstNumber = document.getElementById("firstNumber").value;
   var secondNumber = document.getElementById("secondNumber").value;
-
-  var rest = firstNumber % secondNumber;
-
-
-
-  alert("Résultat :"+rest);
+  if ((isNaN(secondNumber)) || (isNaN(firstNumber))){ //verification que les champs contiennent des nombres
+    alert("Veuillez saisir un nombre.")
+  }
+  else {
+    var rest = firstNumber % secondNumber;
+    alert("Résultat :"+rest);
+  }
 }
+ 
